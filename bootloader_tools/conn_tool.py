f2c008d953a5c053f3e4a2336e3de793d3bfc645
sRGB <srgb565@gmail.com>
2026-04-21 14:17:42 -0400

    fix: payloadfs returned wrong file attributes, preventing boot

--- bootloader_tools/conn_tool.py
+++ bootloader_tools/conn_tool.py
@@ -3,17 +3,17 @@ import threading
 
 
 print("attempting to open device...")
-rcm = uart_rcm.open_device("COM3")
+rcm = uart_rcm.open_device("/dev/ttyUSB0")
 
 if not uart_rcm.bootrom_is_ready(rcm):
     raise OSError("bootrom didn't accept handshake")
 
 print("connection succeeded; device waiting for commands...")
 
-#uart_rcm.boot_payload(rcm, "./payloads/boot_nvs_imager.mpy")
+uart_rcm.boot_payload(rcm, "./payloads/boot_nvs_imager.mpy")
 
 # ATTEMPT NOR BOOT
-uart_rcm.boot_payload(rcm, "./payloads/workaround_nor_boot.mpy")
+#uart_rcm.boot_payload(rcm, "./payloads/workaround_nor_boot.mpy")
 
 # UPDATE ROM:
 #uart_rcm.boot_payload(rcm, "../gen_imager.mpy")
